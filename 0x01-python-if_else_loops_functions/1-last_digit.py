d89a7ca8cda402e0ef3ddbeaa7af72a0c7c26e79
IbetheRamada <valerdagd@gmail.com>
2021-09-08 21:06:22 -0500

    add the 1 file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,18 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    lastn = number % - 10
-    if (lastn > 5):
-        print('Last digit of {} ' .format(number), 'is {} an is greater than 5'.format(lastn))
-    elif (lastn == 0):
-        print('Last digit of {} ' .format(number), 'is {} and is zero' .format(lastn))
-    elif (lastn < 5):
-        print('Last digit of {} ' .format(number), 'is {} and is less than 6 and not 0' .format(lastn))
+    lastn = number % -10
 else:
     lastn = number % 10
-    if (lastn > 5):
-        print('Last digit of {} ' .format(number), 'is {} an is greater than 5'.format(lastn))
-    elif (lastn == 0):
-        print('Last digit of {} ' .format(number), 'is {} and is zero' .format(lastn))
-    elif (lastn < 5):
+
+if (lastn > 5):
+    print('Last digit of {} ' .format(number), 'is {} an is greater than 5'.format(lastn))
+elif (lastn == 0):
+    print('Last digit of {} ' .format(number), 'is {} and is zero' .format(lastn))
+elif (lastn < 5):
         print('Last digit of {} ' .format(number), 'is {} and is less than 6 and not 0' .format(lastn))
