3f886e2f79ceca40c687abf1f75b45c33c93ad33
IbetheRamada <valerdagd@gmail.com>
2021-09-08 21:00:49 -0500

    add the 1 file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,16 @@ number = random.randint(-10000, 10000)
 if (number < 0):
     lastn = number % - 10
     if (lastn > 5):
-        print("Last digit of {} " .format(number), "is {} an is greater than 5".format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} an is greater than 5'.format(lastn))
     elif (lastn == 0):
-        print("Last digit of {} " .format(number), "is {} and is zero".format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} and is zero' .format(lastn))
     elif (lastn < 5):
-        print("Last digit of {} " .format(number), "is {} and is less than 6 and not 0" .format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} and is less than 6 and not 0' .format(lastn))
 else:
     lastn = number % 10
     if (lastn > 5):
-        print("Last digit of {} " .format(number), "is {} an is greater than 5".format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} an is greater than 5'.format(lastn))
     elif (lastn == 0):
-        print("Last digit of {} " .format(number), "is {} and is zero".format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} and is zero' .format(lastn))
     elif (lastn < 5):
-        print("Last digit of {} " .format(number), "is {} and is less than 6 and not 0" .format(lastn))
+        print('Last digit of {} ' .format(number), 'is {} and is less than 6 and not 0' .format(lastn))
