87c9efdc3dfc1f5d2e1050a9b6d5c4073de46e62
IbetheRamada <valerdagd@gmail.com>
2021-09-21 11:46:26 -0500

    Add a new directory andd its 0 and readme file

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(5):
-        print("{}" str.format(my_list[i]))
+        for elm in my_list:
+                print("{:d}".format(elm))
