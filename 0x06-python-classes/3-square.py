31998f164e447949950aaaf4e18dc2b11da098d9
IbetheRamada <valerdagd@gmail.com>
2021-09-28 21:40:28 -0500

    add README file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-3. Area of a square
-"""
+"""3. Area of a square"""
 
 
 class Square:
+    """Write a class Square that defines a square by:
+    (based on 2-square.py)
+    """
     def __init__(self, size=0):
-        """
-        Write a class Square that defines a square by:
-        (based on 2-square.py)
-        """
-        if type(size) != int:
-            raise Exception('size must be an integer')
+        if isinstance(size, int) is False:
+            raise Exception("size must be an integer")
         elif size < 0:
-            raise Exception('size must be >= 0')
+            raise Exception("size must be >= 0")
         self.__size = size
 
     def area(self):
-        size = self.__size
-        return size * size
+        return (self.__size * self.__size)
