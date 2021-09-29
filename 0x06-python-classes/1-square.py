a262ae575278cfb1c378c011805b5230df5312b6
IbetheRamada <valerdagd@gmail.com>
2021-09-28 19:18:18 -0500

    add README file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+1. Square  with size
+"""
+
+
 class Square:
+    """
+    __init__
+    The __init__ method initializes the size value
+    of the square.
+    Attributes:
+    size (int): The size of the square.
+    """
     def __init__(self, size):
         self.__size = size
