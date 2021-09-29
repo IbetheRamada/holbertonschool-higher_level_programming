31486b7f26671374083cb037d458c5e4be2cf5f3
IbetheRamada <valerdagd@gmail.com>
2021-09-28 21:21:40 -0500

    add README file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,11 +14,10 @@ class Square:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
         """
-
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError('size must be an integer')
-
-        if size < 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
 
-        self.__size = size
