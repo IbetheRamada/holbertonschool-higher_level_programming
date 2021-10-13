ce27c387813d32e91d76e735bf5ded2a5774e5e8
IbetheRamada <valerdagd@gmail.com>
2021-10-13 02:21:17 -0500

    add 0 and 1 files

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that returns True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return true if the object is an instance of a class
+    that inherited (directly or indirectly) from the
+    specified class; otherwise False.
+    """
+    if type(obj) is a_class:
+        return False  
+    else:
+        return isinstance(obj, a_class)
+
