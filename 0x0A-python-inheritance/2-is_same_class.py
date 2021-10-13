373b47fec0f0a5805893e8e895b4800389d4620e
IbetheRamada <valerdagd@gmail.com>
2021-10-13 01:50:53 -0500

    add 0 and 1 files

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a function that returns True if the object is exactly an instance of the specified
+a function that returns True if the
+object is exactly an instance of the specified
 class; otherwise False
 """
 
