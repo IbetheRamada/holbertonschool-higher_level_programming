2693d9e753ed5a626efa52e5281cd97d5b0b08f1
IbetheRamada <valerdagd@gmail.com>
2021-10-19 22:51:51 -0500

    add 5 task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+====================
+Function with an empty class BaseGeometry.
+====================
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class BaseGeometry
+    """
