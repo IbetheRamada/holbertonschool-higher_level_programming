d903d7eb3463ed5ea023878bcd20611bff15f572
IbetheRamada <valerdagd@gmail.com>
2021-12-15 18:23:32 -0500

    Add 1 task

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const first_line = 'C is fun';
+const sec_line = 'Python is cool';
+const final_line = 'JavaScript is amazing';
+console.log(first_line);
+console.log(sec_line);
+console.log(final_line);
