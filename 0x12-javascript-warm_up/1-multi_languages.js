f9682422d5828426050460869e224621530d78fd
IbetheRamada <valerdagd@gmail.com>
2021-12-15 18:29:02 -0500

    Add 1 task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const first_line = 'C is fun';
-const sec_line = 'Python is cool';
-const final_line = 'JavaScript is amazing';
-console.log(first_line);
-console.log(sec_line);
-console.log(final_line);
+const line = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(line);
